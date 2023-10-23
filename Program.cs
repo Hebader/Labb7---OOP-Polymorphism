4294221477b942e26cbf61f0ef3a8bca190e2d0c
Heba Derawi <heba.derawi95@hotmail.com>
2023-10-23 15:05:16 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Labb7___OOP_Polymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public class Geometri 
+    {
+    public void Area()
+        {
+
+        }
+    }
+    public class Rektangel: Geometri 
+    {
+    }
+    public class Fyrkant : Geometri
+    {
+    }
+    public class Cirkel : Geometri
+    {
+    }
+    public class Parallellogram : Geometri
+    {
+    }
+    public class Ellips : Geometri
+    {
+    }
+}
